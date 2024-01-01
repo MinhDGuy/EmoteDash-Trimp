d356fbd6ea059f6ddda3e5dff3f209bc3db9efcc
MinhDevGuy <104999039+MinhDGuy@users.noreply.github.com>
2024-01-01 19:17:46 +0700

    Update et.cpp

--- cpp/et.cpp
+++ cpp/et.cpp
@@ -3,6 +3,10 @@
 using namespace std;
 
 int main() {
+	wstring username;
+    wcout << L"Enter Your Roblox Username(It only works when you enter the correct account name you are playing Roblox with): ";
+    wcin >> username;
+    
     const wchar_t* filePath = L"source\\EmoteTrimp.ahk";
 
     HINSTANCE result = ShellExecuteW(NULL, L"open", filePath, NULL, NULL, SW_SHOWNORMAL);
@@ -12,6 +16,6 @@ int main() {
     } else {
         wcout << L"Error executing AutoHotkey script." << endl;
     }
-
-    return 0;
+	
+	return 0;
 }
